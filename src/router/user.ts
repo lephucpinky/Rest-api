bc45732a2734383340420bc3f4047280cb7fcffc
lephucpinky <phuc44654@gmail.com>
2024-10-17 10:12:16 +0700

    sua phan router cua user

--- src/router/user.ts
+++ src/router/user.ts
@@ -9,8 +9,8 @@ let {
 
 
 export default (router:express.Router) => {
-    router.get("/users", isAuthenticated, isAdmin, allUsers); // /api/users
-    router.get("/users/:id", isAuthenticated, singleUser); // /api/users/id
-    router.put("/users/edit/:id", isAuthenticated, editUser); // /api/users/edit/id
-    router.delete("/users/admin/delete/:id", isAuthenticated, isAdmin, deleteUser); // /api/users/admin/delete/id
+    router.get("/users", isAuthenticated, isAdmin, allUsers); 
+    router.get("/users/:id", isAuthenticated, singleUser); 
+    router.put("/users/edit/:id", isAuthenticated, editUser); 
+    router.delete("/users/admin/delete/:id", isAuthenticated, isAdmin, deleteUser);
 }
